3c0e8bf021066199d8074ef2128a1a723afd77cf
Mattie Behrens <mattie.behrens@atomicobject.com>
2023-02-17 16:07:00 -0500

    update example

--- examples/default-vpc.tf
+++ examples/default-vpc.tf
@@ -23,7 +23,8 @@ variable "tailscale_auth_key" {
 }
 
 module "router" {
-  source = "../"
+  source  = "atomicobject/tailscale-router/aws"
+  version = ">= 1.0.0, < 2.0.0"
 
   name               = "tailscale-subnet-router"
   tailscale_auth_key = var.tailscale_auth_key
